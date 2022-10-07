1b45c106f13d7d12851f360c42168f0f229d9883
Anderson Santos <andersonst.dev@gmail.com>
2022-10-07 14:22:27 -0300

    feat: create autocomplete component

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+import Autocomplete from "./components/Autocomplete";
+import { callDataExample } from "./utils";
 
 function App() {
+  const [selectedOption, setSelectedOption] = useState('')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Autocomplete
+        callData={callDataExample}
+        onGetSelectedValue={setSelectedOption}
+      />
+      {!!selectedOption.trim() && (
+        <pre>
+          Selected value: <b>{selectedOption}</b>
+        </pre>
+      )}
+    </main>
   );
 }
 
---
+++ src/components/Autocomplete/index.tsx
@@ -0,0 +1,187 @@
+import { useRef, useState, ChangeEvent, KeyboardEvent } from 'react'
+
+import './styles.css'
+
+const STATUS = {
+  idle: 'idle',
+  loading: 'loading',
+  empty: 'empty'
+}
+
+type AutocompleteSearchProps = {
+  keyValue?: string;
+  callData: (term: string) => Promise<string[]>,
+  debounceMs?: number
+  onGetSelectedValue: (selectedOption: string) => void,
+  placeholder?: string,
+  loadingMsg?: string,
+  emptyMsg?: string
+}
+
+export default function Autocomplete({ 
+  callData,
+  debounceMs = 500,
+  onGetSelectedValue,
+  placeholder = 'Search...',
+  loadingMsg = 'Loading...',
+  emptyMsg = 'No results.'
+}: AutocompleteSearchProps) {
+  const timeoutRef = useRef<ReturnType<typeof setTimeout>>()
+  const [term, setTerm] = useState<string>('') 
+  const [data, setData] = useState<string[]>([])
+  const [status, setStatus] = useState<string>(STATUS.idle)
+
+  function handleReset() {
+    setTerm('')
+    setData([])
+    setStatus(STATUS.idle)
+    onGetSelectedValue('')
+  }
+
+  function handleSelectOption(item: string) {
+    setTerm(item)
+    setData([])
+    setStatus(STATUS.idle)
+    onGetSelectedValue(item)
+  }
+
+  function handleKeyPress(e: KeyboardEvent<HTMLInputElement>) {
+    if (e.code === 'Enter') {
+      onGetSelectedValue(term)
+      setStatus(STATUS.idle)
+      setData([])
+    }
+  }
+
+  function handleChangeInputValue(e: ChangeEvent<HTMLInputElement>) {
+    setStatus(STATUS.loading)
+    setTerm(e.target.value)
+    const currentTerm = e.target.value
+
+    timeoutRef.current && clearTimeout(timeoutRef.current);
+
+    if (!currentTerm.trim()) {
+      handleReset()
+      return;
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      callData(currentTerm)
+        .then(result => {
+          if (!result.length) {
+            setStatus(STATUS.empty)
+          } else {
+            setStatus(STATUS.idle)
+          }
+          setData(result)
+        })
+    }, debounceMs)
+  }
+
+  const showEmptyState = status === STATUS.empty 
+  const showLoadingState = status === STATUS.loading 
+  const showListOptions = !showLoadingState && !!data.length
+  const inputIsFilled = !!term.trim()
+
+  return (
+    <div 
+      className="autocomplete-container" 
+    >
+      <input 
+        type="text" 
+        placeholder={placeholder} 
+        value={term}
+        onChange={handleChangeInputValue}
+        onKeyDown={handleKeyPress}
+      />
+      {inputIsFilled && (
+        <button className='clear-btn' onClick={handleReset}>
+          ✖
+        </button>
+      )}
+      <ul 
+        className="list-options" 
+      >
+        {showLoadingState && <LoadingStatus loadingMsg={loadingMsg} />}
+        {showEmptyState && <EmptyStatus emptyMsg={emptyMsg} />}
+        {showListOptions && (
+          <List
+            data={data}
+            onSelectItem={handleSelectOption}
+            term={term}
+          />
+        )}
+      </ul>
+    </div>
+  )
+}
+
+function LoadingStatus({ loadingMsg }: { loadingMsg: string }) {
+  return (
+    <div className="loading-state">
+      {loadingMsg}
+    </div>
+  )
+}
+
+function EmptyStatus({ emptyMsg }: { emptyMsg: string }) {
+  return (
+    <div className='empty-list'>
+      {emptyMsg}
+    </div>
+  )
+}
+
+type ListProps = {
+  data: string[]
+  onSelectItem: (item: string) => void
+  term: string;
+}
+
+function List({ data, onSelectItem, term }: ListProps) {
+  function handleSelectOption(item: string) {
+    onSelectItem(item)
+  }
+
+  return (
+    <>
+      {data.map(item => (
+        <li key={item}>
+          <button 
+            onClick={() => handleSelectOption(item)}
+          >
+            <Name name={item} highlight={term} />
+          </button>
+        </li>
+      ))}
+    </>
+  )
+}
+
+type NameProps = {
+  name: string
+  highlight: string;
+}
+
+function Name({ name, highlight }: NameProps) {
+  if (!highlight.trim()) return <span>{name}</span>;
+
+  const escapeRegExp = (str = "") =>
+    str.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1");
+  const regex = new RegExp(`(${escapeRegExp(highlight)})`, "gi");
+  const parts = name.split(regex);
+
+  return (
+    <span>
+      {parts
+        .filter((part: string) => part)
+        .map((part: string, i) =>
+          regex.test(part) ? (
+            <b key={i}>{part}</b>
+          ) : (
+            <span key={i}>{part}</span>
+          )
+        )}
+    </span>
+  );
+};
